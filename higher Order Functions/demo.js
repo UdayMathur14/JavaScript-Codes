c209661d1cdbf69faa5968bc0782e1c24aadff9f
udaymathur <udaymathur14@gmail.com>
2023-05-30 18:55:01 +0530

    demo add

---
+++ higher Order Functions/demo.js
@@ -0,0 +1,8 @@
+// higher order function = function which operates on another function , means yeh saare kuch function input lete h and then uspe operate krte h 
+
+let arr = [1,2,3,4,5];
+
+for(let i in arr){
+    arr[i] = arr[i]*2;
+}
+console.log(arr);
