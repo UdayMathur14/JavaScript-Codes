eec185ea9db71dfb3818dfa1484a01c04ea10556
udaymathur <udaymathur14@gmail.com>
2023-08-18 00:57:07 +0530

    math function update

--- script20.js
+++ script20.js
@@ -1,23 +1,23 @@
-// kisi file m  jaake isko link kr diyo and tb dekhiyo console m isko khi link kra nhi h maine
+// kisi file m  jaake isko link kr diyo
 
 
 // this is called function declaration 
 function square_root(n){
-    return Math.sqrt(n);
+   console.log(Math.sqrt(n))
     // math sqrt ek in build function h 
  }
 //  isme hmne ek no n diya function ko and return kr diya uska square root 
- 
-//  console.log(square_root(10));
-// 
-// this way is called function hoisting 
 // this is called function expression 
+//  console.log(square_root(10));
+
+// this way is called function hoisting 
+
  var sqrt = function(){
     console.log("hi");
     return ;
  }
-square_root(10);
- sqrt(10);
+square_root(100);
+sqrt();
 
 
 //  hositing = when you define function only by using the function keyword not sqrt = function , then so what javascript is do it takes all the function and placed in the top of the code 
