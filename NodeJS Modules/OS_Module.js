cb626897cfbcb6cf91ecf864c46db9b615b830a2
udaymathur <udaymathur14@gmail.com>
2023-08-19 01:51:58 +0530

    Add nosejs modules

---
+++ NodeJS Modules/OS_Module.js
@@ -0,0 +1,14 @@
+// os module = operating system 
+// iska basically yeh kaam h ki aapke os means aapke laptop ki specifications bta ske 
+// suppose koi aapne aap banai kisi high system p tb wo chl rhi h but jaise hi aap usko low system m chalate ho  , mtb chote system ka ki usme specification thode se km h , toh usko bhi hm os module se check kr skte h ki kah p wo nhi chl rha ya kya kami h uss laptop m 
+
+
+const os = require('os')
+
+
+console.log(os.arch()) //articheture konsa h 64 bit or  64  32
+
+console.log(os.platform()) // apka system kis platform p bna hua h 
+
+
+console.log(os.cpus()) // apne cpus ke baare m pta lga skte h 
