26da16efc643c642d0891ef919f7a35d24946848
udaymathur <udaymathur14@gmail.com>
2023-08-19 17:21:32 +0530

    call back function change

--- Nados Tuto/Nados CallbackFunction.js
+++ Nados Tuto/Nados CallbackFunction.js
@@ -1,18 +1,30 @@
 //callback - A function that can be passed to another
-//function as an argumenet so that it can be called later is known as a callback function
+//function as an argument so that it can be called later is known as a callback function
 
 
-// EXAMPLE 1
-function printFirstName(firstName, cb) {
+// EXAMPLE 1                      //cb means call back function 
+function printFirstName(firstName, cb , cb2) {
   console.log(firstName)
   cb('jha')
+  cb2(25)
 }
 
-// function printLastName(lastName){
-//        console.log(lastName)
-// }
-// printFirstName('Neeraj' , printLastName);
-// // printLastName('jha');
+function printLastName(lastName){
+       console.log(lastName)
+}
+function age(c){
+  console.log(c);
+}
+//                 firstname , cb 
+printFirstName('Neeraj' , printLastName , age);
+//khaali ek hi function call kiya aur dusra bhi call ho gya apne aap hi cb m 
+
+
+// Another way  
+
+// printFirstName("Neeraj", function printLastName(lastName) {
+//   console.log(lastName+"jo");
+// });
 
 
 
@@ -24,34 +36,15 @@ function printFirstName(firstName, cb) {
 
 
 
-
-
-
-// EXAMPLE 2 
-
-printFirstName("Neeraj", function printLastName(lastName) {
-  console.log(lastName+"jo");
-});
-
-
-
-
-
-
-
-
-
-
-
-// Example 3 
-function printHello(cb) {
-  console.log("Hello");
-  cb()
+// Example 2 
+// function printHello(cb) {
+//   console.log("Hello");
+//   cb()
  
-}
+// }
 
-function printBye() {
-  console.log("Bye");
-}
+// function printBye() {
+//   console.log("Bye");
+// }
 
-printHello(printBye);
+// printHello(printBye);
