c5bc69645f1c082725a73e680b080c741c2c02b0
udaymathur <udaymathur14@gmail.com>
2023-09-02 01:33:13 +0530

    Make a component

--- React/todo/todo/src/App.js
+++ React/todo/todo/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Todo from './Components/Todo';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Todo/>
     </div>
   );
 }
---
+++ React/todo/todo/src/Components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My todo list </h1>
+      </div>
+    )
+  }
+}
+
+export default Todo
