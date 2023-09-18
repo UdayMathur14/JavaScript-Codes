cff15838bb6e4b1876a8af8ef1d8cfe28f7cb6f6
udaymathur14 <udaymathur14@gmail.com>
2023-09-18 16:04:17 +0530

    changes

--- Nados higher Order Functions/map.js
+++ Nados higher Order Functions/map.js
@@ -10,28 +10,33 @@
 
 // Map
 
-//Map is itslef a function.
-//map takes a callback function as an arg
+//Map is itself is a function.
+//map takes a callback function as an argument
 //map will call the callback functions as many times as the elements in the array
 //map will process every value and will apply the instruction that is inside the callback function
 //map returns a new array
 
-let arr = [2, 5, 9, 10, 11];
+let arr = [2, 5, 9, 10, 11,-7];
 
 let squaredArr = arr.map(function (n) {
+  if(n<0){
+    return -1*n;
+  }
   return n * n;
 });
 
 console.log(squaredArr);
 
-// let nameArr = ["Aniket Raj", "Nayan Jha", "Prashant Pandey"];
+let nameArr = ["Aniket Raj", "Nayan Jha", "Prashant Pandey"];
 
-// let modifiedArr = nameArr.map(function(n){
-//    let partsOfName = n.split(' ')
-//    return partsOfName
-// })
+let modifiedArr = nameArr.map(function(n){
+   let partsOfName = n.split(' ')
+   return partsOfName
+})
 
-// console.log(modifiedArr)
+
+
+console.log(modifiedArr)
 
 const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];
 const inrtToUsd = 74;
