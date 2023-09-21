95eb3564720194abc059a665f450f394cc422d26
gogo <udaymathur16@gmail.com>
2023-09-21 18:03:40 +0530

    changes in code

--- script19.js
+++ script19.js
@@ -1,35 +1,37 @@
 // we can declare variable like this in javascript
-c = 20; // it is a global variable declartiion
-var d = 7; // we use var when it has a function scope
-// let a = 10 ; // we use let when we have a block scope
-// console.log(a);
+c = 20; // it is a global variable declartion
 
-function fun() {
-  let a = 5;
-  if (a == 5) {
-     let f = 10;
-    console.log("inside", f);
-    //  yeh wala toh let print ho gya but neeche wala outside wale m error aa gya
-    // ki let ek block scope h means ek block ek ander hi rahega and wo block tha (if condition)
-  }
-  console.log("outside ", f);
-  //  jaise hi yeh let ko bola ki bhaar print hoja wo nhi ho paya
-}
+var d = 7; // var keyword works in function scope but not in block scope
+let a = 10 ; //Let keyword works in both Block scope and function scope
 
-// function ko call bhi krna hota h
-fun();
 
 // function fun() {
 //   let a = 5;
 //   if (a == 5) {
-//     var f = 10;
-//     console.log(f);
+//      let f = 10;
+//     console.log("inside", f);//this is working fine
+
+//     // ki let ek block scope h means ek block ek ander hi rahega and wo block tha (if condition)
 //   }
-//   console.log(f);
+//   console.log("outside ", f); //Not able to print 
 // }
 
+// function ko call bhi krna hota h
+
+
+function fun() {
+  let a = 5;
+  if (a == 5) {
+    var f = 10;
+    console.log(f);
+  }
+  console.log("neeche wala" , f); // yeh bhaar bhi aur ander bhi print ho rha h var iska mtb bekaar h work nhi kr rha 
+}
+
+fun();
+
 // fun();
-// var kr diya toh ab chl rha h block scope p
+// var kr diya toh ab chl rha h block scope p iska mtb bekaar h var block m use krna 
 
 
 
--- script27.js
+++ script27.js
@@ -6,6 +6,6 @@
 
 let obj2 = Object.create(obj);
 // This means that obj2 inherits properties from obj.
-// In the context of the example you provided, obj2 doesn't have its own properties x and y, but it inherits them from obj. You can access these properties on obj2 as if they belong to obj2, even though they are technically inherited from obj.
+// obj2 doesn't have its own properties x and y, but it inherits them from obj. You can access these properties on obj2 as if they belong to obj2, even though they are technically inherited from obj.
 console.log(obj2); 
 console.log(obj2.x); 
